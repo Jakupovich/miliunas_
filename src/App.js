a1e15a0688047f6e3226b6bcbb98df99841fdf70
Jakupovich <nordinveca@gmail.com>
2023-02-21 22:24:40 +0100

    miliunas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState } from 'react';
+const currentIndex = Math.floor(Math.random() * 3)
 function App() {
+  const [currIndex, setCurrIndex] = useState(0)
+  const [questions, setQuestion] = useState([
+    {
+      title: "In a website browser address bar, what does “www” stand for?",
+      answers: ["Web Wide Web", "Web Random Random", "Random Answer for www", "Randm answer 2"],
+      correctAnswer: "Web Wide Web"
+    },
+    {
+      title: "In what year were the first Air Jordan sneakers released?",
+      answers: [1984, 1992, 1975, 1986],
+      correctAnswer: 1984
+    },
+    {
+      title: "According to Greek mythology, who was the first woman on earth?",
+      answers: ["Sefika", "Eva", "Pandora", "Fatima"],
+      correctAnswer: "Pandora"
+    },
+  ])
+  function checkQuestion(answer) {
+    if (answer === questions[currIndex].correctAnswer) {
+      setCurrIndex(currIndex + 1);
+
+    } else {
+      setCurrIndex(0)
+    }
+  }
+
+  function defaultQuestion() {
+    return questions[currIndex].answers.map((answer) => {
+      return (
+        <div key={answer} className="answers" onClick={() => checkQuestion(answer)}>
+          {answer}
+        </div>
+
+      )
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <img className='logo'></img>
+      <h1 className='title'>{questions[currIndex].title}</h1>
+      <div className='answerss  '>{defaultQuestion()}</div>
     </div>
   );
 }
-
 export default App;
